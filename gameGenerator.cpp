564a68e672ebe377eaf3fb5fbc8abe28656a0fd6
Niklas Günther <niklasguenther@outlook.com>
2024-10-20 15:14:01 +0200

    DEV: Initial setup of the algorithm and readme

---
+++ gameGenerator.cpp
@@ -0,0 +1,14 @@
+/* main.cpp */
+#include <iostream>
+#include "gameGenerator.hpp"
+
+// Function definition for printing Hello, World!
+void printHelloWorld() {
+    std::cout << "Hello, World!" << std::endl;
+}
+
+int main() {
+    // Call the function to print Hello, World!
+    printHelloWorld();
+    return 0;
+}
---
+++ gameGenerator.hpp
@@ -0,0 +1,8 @@
+/* gameGenerator.hpp */
+#ifndef GAMEGENERATOR_HPP
+#define GAMEGENERATOR_HPP
+
+// Function declaration to print Hello, World!
+void printHelloWorld();
+
+#endif // HELLO_HPP
